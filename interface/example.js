7e125c3c1219535b34c9abd7a2324caf23c2b651
Saloni197 <Kavita.Goyal@technocodz.com>
2021-07-16 15:16:12 +0530

    typescript
    Signed-off-by: Saloni197 <Kavita.Goyal@technocodz.com>

---
+++ interface/example.js
@@ -0,0 +1,11 @@
+var employee = {
+    fname: "Kavita",
+    lname: "Goyal",
+    age: 24
+};
+var employee2 = {
+    fname: "akash",
+    lname: "yadav"
+};
+console.log(employee);
+console.log(employee2);
---
+++ interface/example.ts
@@ -0,0 +1,17 @@
+interface Person{
+    fname : string
+    lname : string
+    age? : number
+}
+
+let employee : Person = {
+    fname : "Kavita",
+    lname : "Goyal",
+    age : 24
+}
+let employee2 : Person = {
+    fname : "akash",
+    lname : "yadav"
+}
+console.log(employee)
+console.log(employee2);
